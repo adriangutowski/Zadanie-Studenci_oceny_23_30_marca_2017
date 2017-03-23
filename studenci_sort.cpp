6bec4ba19bb05f0cb9043f53702111d39a02500c
Adrian Gutowski <gutowskiap@gmail.com>
2017-03-23 17:41:05 +0100

    Dodano rozwiazanie 2 zadania.

---
+++ studenci_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Student
+{
+    string Nazwisko;
+    float ZIOP, SO, Grafika, MySql;
+    Student()
+    {
+
+    }
+    Student(string Nazwisko, float ZIOP, float SO, float Grafika, float MySql)
+    {
+        this->Nazwisko = Nazwisko;
+        this->ZIOP = ZIOP;
+        this->SO = SO;
+        this->Grafika = Grafika;
+        this->MySql = MySql;
+    }
+    float Srednia()
+    {
+        return (ZIOP + SO + Grafika + MySql) / 4;
+    }
+};
+
+bool operator<(Student &s1, Student &s2)
+{
+    return s2.Srednia() < s1.Srednia();
+}
+
+int main()
+{
+    Student Klasa[5];
+    Klasa[0] = Student("Gutowski", 4, 4.5, 4, 3.5);
+    Klasa[1] = Student("Dawid", 3, 3.5, 3, 4);
+    Klasa[2] = Student("Jaworowski", 3.5, 4, 4, 3.5);
+    Klasa[3] = Student("Walkiewcz", 5, 4.5, 5, 4);
+    Klasa[4] = Student("Kopanski", 2, 2, 2, 2);
+
+    cout.precision(3);
+
+    cout << "TABLICA PRZED SORTOWANIEM:" << endl;
+
+    for( int i = 0; i < 5; ++i)
+    {
+        cout << Klasa[i].Nazwisko << " " << Klasa[i].Srednia() << endl;
+    }
+
+    sort(Klasa, Klasa + 5);
+
+    cout << endl << "TABLICA PO SORTOWANIU:" << endl;
+
+    for( int i = 0; i < 5; ++i)
+    {
+        cout << Klasa[i].Nazwisko << " " << Klasa[i].Srednia() << endl;
+    }
+
+    return 0;
+}
